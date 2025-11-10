0ac96aa59f798fd3115780117556829233045be1
Nikita <groz.yt56@gmail.com>
2025-11-10 21:32:32 +0300

    fix install

--- install.sh
+++ install.sh
@@ -4,22 +4,21 @@ set -e
 
 [[ $EUID -eq 0 ]] || exec sudo "$0" "$@"
 
-repodir=$(dirname "$0")
+chmod +x ./*.sh
 
-chmod +x "$repodir"/*.sh
+"./base.sh"
+"./system.sh"
+"./vim.sh"
+"./yazi.sh"
+"./golang.sh"
+"./3proxy.sh"
+"./xraycore.sh"
 
-"$repodir/base.sh"
-"$repodir/system.sh"
-"$repodir/vim.sh"
-"$repodir/yazi.sh"
-"$repodir/golang.sh"
-"$repodir/3proxy.sh"
-"$repodir/xraycore.sh"
+source "./newuser.sh"
 
-source "$repodir/newuser.sh"
-su - "$NEW_USERNAME" -c "
-    '$repodir/ssh.sh' &&
-    '$repodir/vim.sh' &&
-    '$repodir/yazi.sh' &&
-    '$repodir/golang.sh'
-"
+abcrepodir=$(dirname "$(realpath "$0")")
+
+su - "$NEW_USERNAME" -c "bash '$abcrepodir/ssh.sh'"
+su - "$NEW_USERNAME" -c "bash '$abcrepodir/vim.sh'"
+su - "$NEW_USERNAME" -c "bash '$abcrepodir/yazi.sh'"
+su - "$NEW_USERNAME" -c "bash '$abcrepodir/golang.sh'"
---
+++ tes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+repodir=$(dirname "$(realpath "$0")")
+
+echo "$repodir"
+
+repodir=$(dirname "$0")
+
+echo "$repodir"
