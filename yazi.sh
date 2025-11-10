77e98730ac646c5f8213a1d04ef2af0c32bce70e
Nikita <groz.yt56@gmail.com>
2025-11-10 20:48:11 +0300

    fix yazi

--- yazi.sh
+++ yazi.sh
@@ -7,8 +7,8 @@ set -e
 if ! command -v yazi >/dev/null 2>&1; then
 	wget -O yazi.zip https://github.com/sxyazi/yazi/releases/latest/download/yazi-x86_64-unknown-linux-gnu.zip
 	unzip yazi.zip -d yazi-temp
-	sudo mv yazi-temp/yazi /usr/local/bin/
-    sudo mv yazi-temp/ya /usr/local/bin/
+	sudo mv yazi-temp/yazi-x86_64-unknown-linux-gnu/yazi /usr/local/bin/
+    sudo mv yazi-temp/yazi-x86_64-unknown-linux-gnu/ya /usr/local/bin/
 	rm -rf yazi-temp yazi.zip
 fi
 
